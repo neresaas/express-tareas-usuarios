9ab09c6297824dceadd25945ee2963e8e7a570d1
Nerea <neresaas@gmail.com>
2024-02-16 18:10:01 +0100

    ejemplo query

--- app.js
+++ app.js
@@ -11,8 +11,14 @@ let users = [
     { id:1, email:'user1', password:'user1' },
     { id:2, email:'user2', password:'user2' },
 ];
-
+// Envío de datos como query. Buscará alguna de las palabras del text y la posición en la que se encuentre. Para enviar más de un query se utiliza &
 app.get('/tasks', (req, res) => {
+    let text = req.query.text
+    if( text != undefined) {
+        let tasksWithText = tasks.filter( task => task.text.indexOf(text) != -1 )
+        res.send(tasksWithText)
+        return
+    }
     res.send(tasks)
 });
 // Después de la segunda barra va un parámetro, en este caso el parámetro id
@@ -27,6 +33,12 @@ app.get('/tasks/:id', (req, res) => {
 });
 
 app.get('/users', (req, res) => {
+    let email = req.query.email
+    if (email != undefined) {
+        let userWithEmail = users.filter( user => user.email == email)
+        res.send(userWithEmail)
+        return
+    }
     res.send(users)
 });
 
